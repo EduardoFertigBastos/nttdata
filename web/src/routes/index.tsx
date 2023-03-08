a33528a41bebe2265e098680717ddd326644e4f2
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2023-03-08 19:45:06 -0300

    route

---
+++ web/src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Route, Routes as SwitchRoutes } from 'react-router-dom';
+
+import Home from '../pages/Home';
+
+const Routes: React.FC = () => (
+  <SwitchRoutes>
+    <Route path="/" element={<Home />} />
+    <Route path="*" element={<Home />} />
+  </SwitchRoutes>
+);
+
+export default Routes;
